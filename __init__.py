97c476a4abacff4dcb95a8d6c35d221217bf02e4
Jordan Green <thegreenjordan@gmail.com>
2016-07-30 23:23:37 -0400

    Create template for showing lists

--- __init__.py
+++ __init__.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    user = {'name': 'Jordan'}  # fake user
+    lists = [
+        {'author': {'name': 'Jordan'},
+         'title': 'Shopping List',
+         'items': ['Eggs', 'Milk'],
+         'ordered': False
+         },
+        {'author': {'name': 'Jordan'},
+         'title': 'Schedule',
+         'items': ['Eat', 'Sleep'],
+         'ordered': True
+         }
+        ]
+
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           lists=lists)
 
 
 if __name__ == '__main__':
