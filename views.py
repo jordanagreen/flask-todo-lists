07e5cd061fe97c9c0f956b21a47bbe632cd62ad2
Jordan Green <thegreenjordan@gmail.com>
2016-07-31 11:45:58 -0400

    Create login form

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
 
 
 @app.route('/')
@@ -23,3 +24,15 @@ def index():
                            title='Home',
                            user=user,
                            lists=lists)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login for name %s, remember=%s' %
+              (form.name.data, form.remember_me.data))
+        return redirect('index')
+    return render_template('login.html',
+                           title='Sign in',
+                           form=form)
